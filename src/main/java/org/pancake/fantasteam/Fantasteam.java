66e09aedf52c1d520008fa90e0345ea91f5c771f
Cibernet <41699236+Cibernet83@users.noreply.github.com>
2025-01-10 10:14:12 -0500

    AGAIN!

--- src/main/java/org/pancake/fantasteam/Fantasteam.java
+++ src/main/java/org/pancake/fantasteam/Fantasteam.java
@@ -2,6 +2,7 @@ package org.pancake.fantasteam;
 
 import com.mojang.logging.LogUtils;
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.renderer.entity.EntityRenderers;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.world.food.FoodProperties;
 import net.minecraft.world.item.BlockItem;
@@ -13,6 +14,7 @@ import net.minecraft.world.level.block.Blocks;
 import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.material.MapColor;
 import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.event.BuildCreativeModeTabContentsEvent;
 import net.minecraftforge.event.server.ServerStartingEvent;
@@ -27,6 +29,7 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 import net.minecraftforge.registries.RegistryObject;
+import org.pancake.fantasteam.entities.renderer.SpinProjectileRenderer;
 import org.pancake.fantasteam.registries.ModBlocks;
 import org.pancake.fantasteam.registries.ModCreativeModeTabs;
 import org.pancake.fantasteam.registries.ModEntities;
@@ -35,6 +38,7 @@ import org.slf4j.Logger;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(Fantasteam.MODID)
+@Mod.EventBusSubscriber(bus = Mod.EventBusSubscriber.Bus.MOD)
 public class Fantasteam {
 
 	// Define mod id in a common place for everything to reference
@@ -49,6 +53,7 @@ public class Fantasteam {
 		// Register ourselves for server and other game events we are interested in
 		MinecraftForge.EVENT_BUS.register(this);
 
+
 		ModItems.REGISTRY.register(modEventBus);
 		ModCreativeModeTabs.REGISTRY.register(modEventBus);
 		ModBlocks.REGISTRY.register(modEventBus);
@@ -56,5 +61,12 @@ public class Fantasteam {
 
 		// Register our mod's ForgeConfigSpec so that Forge can create and load the config file for us
 		//ModLoadingContext.get().registerConfig(ModConfig.Type.COMMON, Config.SPEC);
+	}
+
+	@OnlyIn(Dist.CLIENT)
+	@SubscribeEvent
+	public static void onClientSetup(FMLClientSetupEvent event)
+	{
+		EntityRenderers.register(ModEntities.VASTAYAN_FEATHER.get(), SpinProjectileRenderer::new);
 	}
 }
